23b8a4c28c10968353c92843228fe5a2000d83d4
ynoydelk <144928977+ynoydelk@users.noreply.github.com>
2024-10-09 14:34:24 -0300

    atualizações de divs, header e sections
    mudando várias coisas...

--- javascript/script.js
+++
@@ -1,9 +0,0 @@
-let currentSlide = 0;
-
-function moveCarousel(direction) {
-  const carousel = document.querySelector('.carousel');
-  const items = document.querySelectorAll('.carousel-item');
-  currentSlide = (currentSlide + direction + items.length) % items.length;
-
-  carousel.style.transform = `translateX(-${currentSlide * 100}%)`;
-}
